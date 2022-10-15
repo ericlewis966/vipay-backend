65d778ed7759418a00110bf9c4380d9a26a3f8de
cyber-storm-200712 <leonsummer2007@gmail.com>
2022-10-15 20:58:29 +0900

    second commit

--- src/routes/V1/index.js
+++ src/routes/V1/index.js
@@ -1,4 +1,4 @@
-import FileTransferRoutes from './fileTransfer';
+// import FileTransferRoutes from './fileTransfer';
 import SessionRoutes from './session';
 import userRoutes from './user';
-export const Routes_v1 = [...FileTransferRoutes, ...SessionRoutes, ...userRoutes];
+export const Routes_v1 = [/*...FileTransferRoutes,*/ ...SessionRoutes, ...userRoutes];
